114bc15ea338b99f6d79294b224f705dce34d4f4
cramer86 <cramer86@gmail.com>
2018-11-22 13:03:14 +0100

    Feature/new_hashing (#6)
    new hashing + random algorithms
    added AES CBC class and HMACSha

---
+++ locksmith/src/main/java/dk/nodes/locksmith/core/encryption/providers/AesCBCPasswordKeyProviderImpl.java
@@ -0,0 +1,54 @@
+package dk.nodes.locksmith.core.encryption.providers;
+
+import java.nio.charset.Charset;
+import java.security.Key;
+import java.security.MessageDigest;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+
+import dk.nodes.locksmith.core.exceptions.LocksmithException;
+
+public class AesCBCPasswordKeyProviderImpl implements KeyProvider {
+    private Charset charSet = Charset.forName("UTF-8");
+    private String keyType = "AES";
+    private String hashAlgorithm = "SHA-256";
+    private String cipherType = "AES/CBC/PKCS5Padding";
+    private String password;
+    private SecretKeySpec secretKeySpec;
+
+    public AesCBCPasswordKeyProviderImpl(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public void init() throws LocksmithException {
+        try {
+            MessageDigest digest = MessageDigest.getInstance(hashAlgorithm);
+            byte[] bytes = password.getBytes(charSet);
+            digest.update(bytes);
+            byte[] key = digest.digest();
+            secretKeySpec = new SecretKeySpec(key, keyType);
+        } catch (Exception e) {
+            throw new LocksmithException(LocksmithException.Type.Initiation, e);
+        }
+    }
+
+    @Override
+    public Key getKey() throws LocksmithException {
+        if (secretKeySpec == null) {
+            throw new LocksmithException(LocksmithException.Type.Uninitiated);
+        }
+
+        return secretKeySpec;
+    }
+
+    @Override
+    public Cipher getCipher() throws LocksmithException {
+        try {
+            return Cipher.getInstance(cipherType);
+        } catch (Exception e) {
+            throw new LocksmithException(LocksmithException.Type.Initiation, e);
+        }
+    }
+}
--- locksmith/src/main/java/dk/nodes/locksmith/core/util/HashingUtils.java
+++ locksmith/src/main/java/dk/nodes/locksmith/core/util/HashingUtils.java
@@ -1,25 +1,49 @@
 package dk.nodes.locksmith.core.util;
 
+import android.util.Base64;
+
+import java.io.UnsupportedEncodingException;
 import java.nio.charset.Charset;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+
+import dk.nodes.locksmith.core.exceptions.LocksmithException;
 
 public class HashingUtils {
     private static Charset DEFAULT_CHARSET = Charset.forName("UTF-8");
 
     public static String sha512(String value) {
-        return hashString("SHA-512", value);
+        return bytesToHex(hashString("SHA-512", value));
     }
 
-    public static String sha256(String value) {
+    public static String sha512AsBase64(String value) {
+        return bytesToBase64(hashString("SHA-512", value));
+    }
+
+    public static byte[] sha256AsByteArray(String value) {
         return hashString("SHA-256", value);
     }
 
-    public static String sha1(String value) {
-        return hashString("SHA-1", value);
+    public static String sha256(String value) {
+        return bytesToHex(hashString("SHA-256", value));
     }
 
-    private static String hashString(String type, String value) {
+    public static String sha256AsBase64(String value) {
+        return bytesToBase64(hashString("SHA-256", value));
+    }
+
+    public static String sha1(String value) {
+        return bytesToHex(hashString("SHA-1", value));
+    }
+
+    public static String sha1AsBase64(String value) {
+        return bytesToBase64(hashString("SHA-1", value));
+    }
+
+    private static byte[] hashString(String type, String value) {
         try {
             byte[] valueBytes = value.getBytes(DEFAULT_CHARSET);
 
@@ -27,10 +51,14 @@ public class HashingUtils {
                     .getInstance(type)
                     .digest(valueBytes);
 
-            return bytesToHex(digestBytes);
+            return digestBytes;
         } catch (NoSuchAlgorithmException e) {
-            return value;
+            return value.getBytes(DEFAULT_CHARSET);
         }
+    }
+
+    private static String bytesToBase64(byte[] hash) {
+        return Base64.encodeToString(hash, Base64.NO_WRAP);
     }
 
     private static String bytesToHex(byte[] hash) {
@@ -44,4 +72,27 @@ public class HashingUtils {
 
         return hexString.toString();
     }
+
+    public static String hmacSha256(String password, String value) throws LocksmithException {
+        try {
+            return hmacSha256(password.getBytes("UTF-8"), value);
+        } catch (UnsupportedEncodingException e) {
+            throw new LocksmithException(LocksmithException.Type.EncryptionError, e);
+        }
+    }
+
+    public static String hmacSha256(byte[] password, String value) throws LocksmithException {
+        try {
+            Mac sha256_HMAC = Mac.getInstance("HmacSHA256");
+
+            SecretKeySpec secret_key = new SecretKeySpec(password, "HmacSHA256");
+            sha256_HMAC.init(secret_key);
+
+            byte[] bytes = sha256_HMAC.doFinal(value.getBytes("UTF-8"));
+            return Base64.encodeToString(bytes, Base64.NO_WRAP);
+        } catch (Exception e) {
+            throw new LocksmithException(LocksmithException.Type.EncryptionError, e);
+        }
+    }
+
 }
